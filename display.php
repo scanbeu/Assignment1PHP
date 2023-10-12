54156272045cfee588f10bcaa5fe236294bac97d
scanbeu <136927001+scanbeu@users.noreply.github.com>
2023-10-11 23:08:13 -0400

    Add files via upload

---
+++ display.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Display Data</title>
+    <meta name="description" content="Display student information from the database">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./style.css">
+</head>
+
+<body>
+    <section style="margin-top: 0px;" class="masthead">
+        <div>
+            <h1 style="padding: 5.38px;">STUDENT DATA</h1>
+        </div>
+    </section>
+    <main style="margin-top: -200px;" class="container">
+        <section class="form-row row justify-content-center">
+            <?php
+            // Database connection
+            $db_server = "localhost";
+            $db_username = "root";
+            $db_password = "Sahil#12213456";
+            $db_name = "website_db";
+
+            $conn = new mysqli($db_server, $db_username, $db_password, $db_name);
+
+            // Check connection
+            if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+            }
+
+            // Retrieve and display data
+            $sql = "SELECT * FROM student"; // Corrected table name
+
+            $result = $conn->query($sql);
+
+            if ($result->num_rows > 0) {
+                echo '<table class="table">';
+                echo '<tr><th>ID</th><th>First Name</th><th>Last Name</th><th>Grade</th></tr>';
+                while ($row = $result->fetch_assoc()) {
+                    echo '<tr>';
+                    echo '<td>' . $row['idstudent'] . '</td>';
+                    echo '<td>' . $row['fname'] . '</td>';
+                    echo '<td>' . $row['lname'] . '</td>';
+                    echo '<td>' . $row['grade'] . '</td>';
+                    echo '</tr>';
+                }
+                echo '</table>';
+            } else {
+                echo "No data found.";
+            }
+
+            $conn->close();
+            ?>
+        </section>
+    </main>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Grade Portal</title>
+	<meta name="description" content="Submit student information for grading">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css">
+	<link rel="stylesheet" href="./style.css">
+	
+</head>
+<body>
+	<section style="margin-top: 0px;" class="masthead">
+		<div>
+			<h1 style="padding: 5.38px;">GRADE PORTAL</h1>
+		</div>
+	</section>
+	<main style="margin-top: -200px;"class="container">
+		<section  class="form-row row justify-content-center">
+			<form method="post" action="database.php">
+			<h2 class="mb-3" >STUDENT INFO</h2>
+				<div class="form-group">
+					<label for="idstudent">Student id</label>
+					<input type="text" name="idstudent" id="idstudent" class="form-control">
+				</div>
+				<div class="form-group">
+					<label for="fname">First Name</label>
+					<input type="text" name="fname" id="fname" class="form-control">
+				</div>
+				<div class="form-group">
+					<label for="lname">Last Name</label>
+					<input type="text" name="lname" id="lname" class="form-control">
+				</div>
+				<div class="form-group">
+					<label for="grade">Grade</label>
+					<input type="text" name="grade" id="grade" class="form-control">
+				</div>
+				<div>
+					<button type="submit" class="btn btn-primary">Submit</button>
+				</div>
+				<div style="text-align: center; margin-top: 20px;">
+        			<a href="display.php" class="btn btn-secondary">View Student Data</a>
+    			</div>
+			</form>
+		</section>
+	</main>
+</body>
+</html>
